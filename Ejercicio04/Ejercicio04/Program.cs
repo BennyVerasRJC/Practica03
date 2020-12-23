44a80fe1c7f4d64fc627758bb313353bfb18bf41
BennyVeras <bennyveras214@gmail.com>
2020-12-23 11:00:08 +0100

    Ejercicio 04 OK

---
+++ Ejercicio04/Ejercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 3;
+
+            Console.WriteLine("N incrementada 77 veces= {0}",N+=77);
+            Console.WriteLine("N decrementada 3 veces= {0}",N-=3);
+            Console.WriteLine("N duplicada= {0}",N*2);
+        }
+    }
+}
