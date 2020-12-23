dcdd315d8636e2565b49a9a056a81fe5af7a35c0
BennyVeras <bennyveras214@gmail.com>
2020-12-23 11:05:44 +0100

    Ejercicio 05 OK

---
+++ Ejercicio05/Ejercicio05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 2;
+            int C = 3;
+            int D = 4;
+            int temporal = B;
+
+            B = C;
+            C = A;
+            A = D;
+            D = temporal;
+            Console.WriteLine("A={0}, B={1}, C={2}, D={3}",A,B,C,D);
+        }
+    }
+}
