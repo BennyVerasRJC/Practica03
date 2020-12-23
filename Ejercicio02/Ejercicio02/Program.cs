5661e11643513b2e39419b7e9c484e22054c26a8
BennyVeras <bennyveras214@gmail.com>
2020-12-23 10:36:27 +0100

    Ejercicio 02 OK

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            double A = 5.5;
+            char C = 'R';
+
+            double suma = N + A;
+            double resta = A - N;
+            int valorC = (int)C;
+            Console.WriteLine("N={0}, A={1}, C={2}",N,A,C);
+            Console.WriteLine("N+A= " + suma);
+            Console.WriteLine("A-N= " + resta);
+            Console.WriteLine("C= " + valorC);
+            Console.ReadKey();
+
+        }
+    }
+}
