869550380ea2db9ac021f689fa69b5f115769fe6
BennyVeras <bennyveras214@gmail.com>
2020-12-23 10:30:52 +0100

    Ejercicio01 OK

---
+++ Practica03_Variables/Practica03_Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practica03_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 4;
+
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int multiplicacion = num1 * num2;
+            int division = num1 / num2;
+            int modulo = num1 % num2;
+
+            Console.WriteLine("Suma: {0}, Resta: {1}, Multiplicación: {2}, División: {3}, Módulo: {4}.",suma,resta,multiplicacion,division,modulo);
+
+        }
+    }
+}
